4b070adb63411b8814294ce9213fdadb7bb6fcfb
Luca Morelli <luca.morelli@ovalmoney.com>
2022-01-19 12:36:02 +0100

    feat: add LayoutedContent component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { BaseLayout } from 'components/BaseLayout'
 import { Card } from 'components/Card'
 import { ChangeRate } from 'components/ChangeRate'
+import { LayoutedContent } from 'components/LayoutedContent'
 import { Hero } from 'components/Hero'
 import { Icon } from 'components/Icon'
 
@@ -33,35 +34,10 @@ const Home: NextPage = () => {
         />
         <Icon name="ArrowDown" />
       </Hero>
-      <div className={styles.grid}>
-        <a href="https://nextjs.org/docs" className={styles.card}>
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className={styles.card}>
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/vercel/next.js/tree/canary/examples"
-          className={styles.card}
-        >
-          <h2>Examples &rarr;</h2>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-        >
-          <h2>Deploy &rarr;</h2>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section>
+        <h2>A breakdown of CHSB&rsquo;s circulating supply</h2>
+        <LayoutedContent mobileInverted></LayoutedContent>
+      </section>
     </BaseLayout>
   )
 }
