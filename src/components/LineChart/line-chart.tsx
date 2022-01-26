badda9c66fef303cdb709c53c594cdf57395edec
Luca Morelli <luca.morelli@ovalmoney.com>
2022-01-26 16:34:54 +0100

    feat: add style to LineChart

--- src/components/LineChart/line-chart.tsx
+++ src/components/LineChart/line-chart.tsx
@@ -1,6 +1,11 @@
 import moment from 'moment';
 import React, { useCallback, useRef, useState } from 'react';
-import { AreaChart, LineChart, XAxis, YAxis, CartesianGrid, Tooltip, Area, Line } from 'recharts';
+import { AreaChart, LineChart, XAxis, YAxis, CartesianGrid, Tooltip, Area, Line, ResponsiveContainer } from 'recharts';
+import Tokens from 'styles/tokens.json';
+
+import styles from './line-chart.module.css';
+
+type Tick = ((props: any) => React.ReactElement<SVGElement>);
 
 const data = [
   {
@@ -35,18 +40,30 @@ const data = [
 
 const formatXAxis = (tickItem: string) => moment(tickItem).format('HH:mm');
 
+const formatYAxis = (tickItem: number) => tickItem.toFixed(3);
+
 export const Chart = () => {
-  const [height, setHeight] = useState(null);
-  const [width, setWidth] = useState(null);
-  const div = useCallback(node => {
-    if (node !== null) {
-      setHeight(node.getBoundingClientRect().height);
-      setWidth(node.getBoundingClientRect().width);
-    }
+  const CustomizedYTick: Tick = useCallback(({ fill, height, orientation, stroke, payload: { value }, type, width, x, y, index }) => {
+    if (Number.parseInt(index) !== 0)
+      return (
+        <text
+          orientation={orientation}
+          width={width}
+          height={height}
+          type={type}
+          x={x}
+          y={y + 15}
+          stroke={stroke}
+          fill={Tokens.white}
+          className="recharts-text recharts-cartesian-axis-tick-value"
+          textAnchor='end'>
+          <tspan dx="0.5em" dy="0.355em" className={styles.YTick}>{formatYAxis(value)}</tspan>
+        </text>
+      )
+    return <></>;
   }, []);
 
-
-  const CustomizedTick = ({ fill, height, orientation, stroke, payload: { value }, textAnchor, type, width, x, y }) => (
+  const CustomizedXTick: Tick = useCallback(({ height, orientation, stroke, payload: { value }, textAnchor, type, width, x, y, ...rest }) => (
     <text
       orientation={orientation}
       width={width}
@@ -55,38 +72,53 @@ export const Chart = () => {
       x={x}
       y={y + 15}
       stroke={stroke}
-      fill={fill}
+      fill={Tokens.white}
       className="recharts-text recharts-cartesian-axis-tick-value"
       textAnchor={textAnchor}>
-      <tspan dy="0.355em">{value}</tspan>
+      <tspan className={styles.XTick}>{formatXAxis(value)}</tspan>
     </text>
-  );
+  ), []);
 
-  // <g class="recharts-layer recharts-cartesian-axis recharts-yAxis yAxis"><g class="recharts-cartesian-axis-ticks"><g class="recharts-layer recharts-cartesian-axis-tick"><text orientation="right" width="60" height="360" type="number" x="808" y="370" stroke="none" fill="#666" class="recharts-text recharts-cartesian-axis-tick-value" text-anchor="start"><tspan x="808" dy="0.355em">1.122</tspan></text></g><g class="recharts-layer recharts-cartesian-axis-tick"><text orientation="right" width="60" height="360" type="number" x="808" y="262.00000000000955" stroke="none" fill="#666" class="recharts-text recharts-cartesian-axis-tick-value" text-anchor="start"><tspan x="808" dy="0.355em">1.1235</tspan></text></g><g class="recharts-layer recharts-cartesian-axis-tick"><text orientation="right" width="60" height="360" type="number" x="808" y="154.0000000000032" stroke="none" fill="#666" class="recharts-text recharts-cartesian-axis-tick-value" text-anchor="start"><tspan x="808" dy="0.355em">1.125</tspan></text></g><g class="recharts-layer recharts-cartesian-axis-tick"><text orientation="right" width="60" height="360" type="number" x="808" y="10" stroke="none" fill="#666" class="recharts-text recharts-cartesian-axis-tick-value" text-anchor="start"><tspan x="808" dy="0.355em">1.127</tspan></text></g></g></g>
   return (
-    <div style={{ width: "100%", height: "400px" }} ref={div}>
-      {height && width && <AreaChart height={height} width={width} data={data}
-        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-        <defs>
-          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#01c38d" stopOpacity={0.6} />
-            <stop offset="95%" stopColor="#01c38d" stopOpacity={0} />
-          </linearGradient>
-        </defs>
-        <CartesianGrid horizontal vertical={false} />
-        <XAxis dataKey="time" tickFormatter={formatXAxis} />
-        <YAxis
-          interval="preserveStartEnd"
-          orientation="right"
-          domain={['dataMin', 'dataMax']}
-          axisLine={false}
-          tickLine={false}
-          tickMargin={-50}
-          tick={CustomizedTick}
-        />
-        <Tooltip />
-        <Area type="linear" dataKey="price" stroke="#20C38D" fillOpacity={1} fill="url(#colorPv)" />
-      </AreaChart>}
+    <div style={{ width: "100%", height: "400px" }}>
+      <ResponsiveContainer width="100%">
+        <AreaChart data={data}
+          margin={{ top: 10, right: -60, left: 0, bottom: 0 }}>
+          <defs>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={Tokens.primary} stopOpacity={0.6} />
+              <stop offset="95%" stopColor={Tokens.primary} stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <CartesianGrid horizontal vertical={false} stroke={Tokens.neutral} />
+          <Tooltip />
+          <Area
+            type="linear"
+            dataKey="price"
+            stroke={Tokens.primary}
+            fillOpacity={1}
+            fill="url(#colorPv)" />
+          <XAxis
+            dataKey="time"
+            // tickFormatter is needed because the engine automatically calculates the number of ticks basing on the length of the string values
+            tickFormatter={formatXAxis}
+            tick={CustomizedXTick}
+            stroke={Tokens.neutral}
+            padding={{ right: Tokens.space * 3 }}
+          />
+          <YAxis
+            interval="preserveStartEnd"
+            orientation="right"
+            domain={['dataMin', 'dataMax']}
+            axisLine={false}
+            tickLine={false}
+            tickMargin={-1.33 * Tokens.space}
+            tickFormatter={formatYAxis}
+            tick={CustomizedYTick}
+            stroke={Tokens.neutral}
+          />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   )
 };
